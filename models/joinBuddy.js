8211e772100f20fdc671f7b3011a70329f9465f8
aschaaf <schaaf1091@gmail.com>
2019-06-22 11:21:34 -0600

    added database files

---
+++ models/joinBuddy.js
@@ -0,0 +1,41 @@
+module.exports = (sequelize, DataTypes) => {
+    const Buddy = sequelize.define('Buddy', {
+        duration: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validation: {
+                notEmpty: true
+            }
+        },
+        active: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: true
+        },
+        buddyId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validation: {
+                isNumeric: true,
+                not: ["[a-z]", 'i']
+            }
+        }
+    })
+
+    Buddy.associate = model => {
+        Buddy.belongsTo(model.User, {
+            foreignKey: {
+                allowNull: false,
+            }
+        });
+        Buddy.belongsTo(model.Goals, {
+            foreignKey: {
+                allowNull: false
+            }
+        })
+    }
+
+    return Buddy;
+
+
+}
