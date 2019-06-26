568a7e103efcf9689105eb2ed79ed2465f8ac7cd
aschaaf <schaaf1091@gmail.com>
2019-06-25 21:31:32 -0600

    routes work to pull in user data

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,45 @@
-const router = require("express").Router();
-const userController = require("../controller/userQueries");
-// const buddy = require("../../controller/buddyQueries");
-// const helper = require("../../utils/helperFunctions");
+const user = require("../controller/userQueries");
 
-// Matches with "/api/user"
-router.route("/").get(userController.getAllUsers);
+module.exports = app => {
+  // This route will add a new user to the database
 
-// Matches with "/api/user/:id"
-router.route("/:id").get(userController.findUser);
+  // app.post("/add/user", (req, res) => {
+  //   var firstName = req.body.firstName;
+  //   var lastName = req.body.lastName;
+  //   var username = req.body.username;
+  //   var email = req.body.email;
+  //   var password = req.body.password;
+  //   user
+  //     .addUser(firstName, lastName, username, email, password)
+  //     .then(data => {
+  //       console.log(data);
+  //       res.json(data);
+  //     })
+  //     .catch(err => {
+  //       res.send(err);
+  //     });
+  // });
 
-module.exports = router;
+  // This route will return all users
+  app.get("/all/users", (req, res) => {
+    user
+      .getAllUsers()
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.send(err);
+      });
+  });
+
+  //   app.get("/user/:id", (req, res) => {
+  //     user
+  //       .findUser(req.params.id)
+  //       .then(data => {
+  //         res.json(data);
+  //       })
+  //       .catch(err => {
+  //         res.send(err);
+  //       });
+  //   });
+};
