10eb350f1663ed5305b107316f6478feff219660
aschaaf <schaaf1091@gmail.com>
2019-07-01 14:03:45 -0600

    got goals and milestones to populate

--- client/src/pages/User/index.js
+++ client/src/pages/User/index.js
@@ -15,71 +15,71 @@ class User extends Component {
       buddies: {
         buddiesWith: [],
         myBuddies: [],
-        allBuddies: undefined
-      },
-      goals: {
-        activeGoals: {
-          completed: [],
-          incomplete: []
-        },
-        pastGoals: {
-          completed: [],
-          incomplete: []
-        }
-      },
-      milestones: {
-        activeMilestones: {
-          completed: [],
-          incomplete: []
-        },
-        pastMilestones: {
-          completed: [],
-          incomplete: []
-        }
+        allBuddies: []
       }
-    }
+    },
+    currentGoalsIncomplete: []
   };
 
   // Fetch the list on first mount
   componentDidMount() {
     API.getUserByEmail(this.props.user).then(resp => {
       console.log(resp.data);
-      let data = resp.data;
-      this.setState({
-        userInfo: {
-          firstName: data.firstName,
-          lastName: data.lastName,
-          username: data.username,
-          email: data.email,
-          id: data.id,
-          buddies: {
-            buddiesWith: data.buddies.buddiesWith,
-            myBuddies: data.buddies.myBuddies,
-            allBuddies: data.buddies.allBuddies
-          },
-          goals: {
-            activeGoals: {
-              completed: data.activeGoals.completed,
-              incomplete: data.activeGoals.incomplete
-            },
-            pastGoals: {
-              completed: data.pastGoals.completed,
-              incomplete: data.pastGoals.incomplete
-            }
-          },
-          milestones: {
-            activeMilestones: {
-              completed: data.activeMilestones.completed,
-              incomplete: data.activeMilestones.incomplete
-            },
-            pastMilestones: {
-              completed: data.pastMilestones.completed,
-              incomplete: data.pastMilestones.incomplete
+      let userData = resp.data;
+      API.getAllGoals(userData.id).then(res => {
+        console.log(res.data);
+        let goalData = res.data;
+        this.setState({
+          currentGoalsIncomplete: goalData.currentGoals.incomplete
+        });
+        this.setState({
+          userInfo: {
+            firstName: userData.firstName,
+            lastName: userData.lastName,
+            username: userData.username,
+            email: userData.email,
+            id: userData.id,
+            buddies: {
+              buddiesWith: userData.buddies.buddiesWith,
+              myBuddies: userData.buddies.myBuddies,
+              allBuddies: userData.buddies.allBuddies
             }
           }
-        }
+        });
       });
     });
+  }
+
+  renderGoals() {
+    return this.state.currentGoalsIncomplete.map(goal => (
+      <div className="col s6" key={goal.id}>
+        <div className="card blue-grey darken-1">
+          <div className="card-content white-text">
+            <span className="card-title">{goal.name}</span>
+            <div className="card-action">
+              <Link to="#">Category: {goal.category}</Link>
+              <br />
+              <Link to="#">Due Date: {goal.dueDate}</Link>
+              <br />
+              <button className="btn">Edit Goal</button>
+              <button className="btn">Delete Goal</button>
+            </div>
+            <div>
+              <h5>Milestones</h5>
+              {goal.milestones.incomplete.map(milestone => (
+                <div key={milestone.id}>
+                  <p>Frequency: {milestone.name}</p>
+                  <p>Frequency: {milestone.frequency}</p>
+                  <p>Due Date: {milestone.dueDate}</p>
+                  <hr />
+                </div>
+              ))}
+            </div>
+            <div />
+          </div>
+        </div>
+      </div>
+    ));
   }
 
   addGoal() {
@@ -98,58 +98,34 @@ class User extends Component {
     // insert a route to mark a goal as complete
   }
 
-  filterMilestones() {
-    // create a function to return all the milestones for each specific goal (ask Hunter)
-  }
-
   render() {
-    const userInfo = this.state.userInfo;
     return (
       <>
         <div className="row">
           <h1>{this.props.user}</h1>
           <div className="col s12">
-            <h4>{`${userInfo.firstName} ${userInfo.lastName}`}</h4>
-            <h5>{userInfo.username}</h5>
-            <h5>{userInfo.email}</h5>
+            <h4>{`${this.state.userInfo.firstName} ${
+              this.state.userInfo.lastName
+            }`}</h4>
+            <h5>{this.state.userInfo.username}</h5>
+            <h5>{this.state.userInfo.email}</h5>
           </div>
         </div>
         <hr />
         <div className="row">
-          <h5>Incomplete Active Goals</h5>
+          <h5>Current Incomplete Goals</h5>
           <hr />
-          {userInfo.goals.activeGoals.incomplete.map(goal => (
-            <div className="col s4" key={goal.id}>
-              <div className="card blue-grey darken-1">
-                <div className="card-content white-text">
-                  <span className="card-title">{goal.name}</span>
-                  <div className="card-action">
-                    <Link to="#">{goal.category}</Link>
-                    <Link to="#">{goal.dueDate}</Link>
-                    <br />
-                    <button className="btn">Edit Goal</button>
-                    <button className="btn">Delete Goal</button>
-                  </div>
-                  <div />
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-        <div className="row">
-          <h5>Milestones</h5>
-          <hr />
-          Filter through milestones to return milestones for each specific goal
+          <div>{this.renderGoals()}</div>
         </div>
         <div className="row">
           <h5>allBuddies</h5>
           <hr />
-          {userInfo.buddies.allBuddies}
+          {this.state.userInfo.buddies.allBuddies}
         </div>
         <div className="row">
           <h5>buddiesWith</h5>
           <hr />
-          {userInfo.buddies.buddiesWith.map(buddy => (
+          {this.state.userInfo.buddies.buddiesWith.map(buddy => (
             <div key={buddy.buddyId}>
               <p>Buddy ID: {buddy.buddyId}</p>
               <p>Goal ID: {buddy.goalId}</p>
@@ -162,3 +138,25 @@ class User extends Component {
 }
 
 export default User;
+
+// <div className="row">
+//   <h5>Incomplete Active Goals</h5>
+//   <hr />
+//   {userInfo.goals.activeGoals.incomplete.map(goal => (
+//     <div className="col s4" key={goal.id}>
+//       <div className="card blue-grey darken-1">
+//         <div className="card-content white-text">
+//           <span className="card-title">{goal.name}</span>
+//           <div className="card-action">
+//             <Link to="#">{goal.category}</Link>
+//             <Link to="#">{goal.dueDate}</Link>
+//             <br />
+//             <button className="btn">Edit Goal</button>
+//             <button className="btn">Delete Goal</button>
+//           </div>
+//           <div />
+//         </div>
+//       </div>
+//     </div>
+//   ))}
+// </div>
