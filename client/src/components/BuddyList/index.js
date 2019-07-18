3e18e04f04b19a654bb3eebf34790a9dbaea34ed
jval <joshvaldez3320@gmail.com>
2019-07-17 22:18:22 -0600

    colors added

--- client/src/components/BuddyList/index.js
+++ client/src/components/BuddyList/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import API from "../../utils/API";
+import "./style.css";
 
 const BuddyList = props => {
   const [buddies, setBuddies] = useState([]);
