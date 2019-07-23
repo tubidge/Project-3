87efdc1402af989243901b56a51307d27c1962db
aschaaf <schaaf1091@gmail.com>
2019-07-23 08:52:50 -0600

    fixed cannot read property of allBuddies

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -163,36 +163,39 @@ const Dashboard = () => {
               </h5>
             </Link>
           </div>
-          {!userInfo.buddies.allBuddies ? (
-            <div id="noBuddies">
-              <p>You don't have any Buddies... yet!</p>
-              <p>
-                If you are having a hard time finding Buddies, just{" "}
-                <Link
-                  to="/buddies"
-                  style={{ borderBottom: "1px dashed #2867aa" }}
-                >
-                  click here
-                </Link>{" "}
-                to generate some matches based on your goals!
-              </p>
-              <p>
-                So if you haven't added any goals, you may want to do that
-                first!
-              </p>
-            </div>
-          ) : (
-            <Chat
-              userInfo={userInfo}
-              myBuddies={myBuddies}
-              buddies={allBuddies}
-              buddiesUsername={
-                allBuddies ? getUnique(allBuddies, "username") : null
-              }
-              buddiesEmail={allBuddies ? getUnique(allBuddies, "email") : null}
-              makeid={makeid}
-            />
-          )}
+          {user &&
+            (!userInfo.buddies ? (
+              <div id="noBuddies">
+                <p>You don't have any Buddies... yet!</p>
+                <p>
+                  If you are having a hard time finding Buddies, just{" "}
+                  <Link
+                    to="/buddies"
+                    style={{ borderBottom: "1px dashed #2867aa" }}
+                  >
+                    click here
+                  </Link>{" "}
+                  to generate some matches based on your goals!
+                </p>
+                <p>
+                  So if you haven't added any goals, you may want to do that
+                  first!
+                </p>
+              </div>
+            ) : (
+              <Chat
+                userInfo={userInfo}
+                myBuddies={myBuddies}
+                buddies={allBuddies}
+                buddiesUsername={
+                  allBuddies ? getUnique(allBuddies, "username") : null
+                }
+                buddiesEmail={
+                  allBuddies ? getUnique(allBuddies, "email") : null
+                }
+                makeid={makeid}
+              />
+            ))}
         </div>
         <div style={{ marginTop: "20px", marginBottom: "20px" }} />
         <div className="col l8 s12">{renderGoalCards()}</div>
