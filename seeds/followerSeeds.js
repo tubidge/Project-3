0b8c282a29f61917f3b8f5a3b471ae552315fce6
aschaaf <schaaf1091@gmail.com>
2019-07-23 19:21:52 -0600

    fixing bugs

---
+++ seeds/followerSeeds.js
@@ -0,0 +1,43 @@
+var db = require("../models");
+db.Followers.bulkCreate([
+  {
+    follower: 18,
+    GoalId: 17
+  },
+  {
+    follower: 17,
+    GoalId: 17
+  },
+  {
+    follower: 15,
+    GoalId: 17
+  },
+  {
+    follower: 4,
+    GoalId: 17
+  },
+  {
+    follower: 5,
+    GoalId: 17
+  },
+  {
+    follower: 6,
+    GoalId: 17
+  },
+  {
+    follower: 7,
+    GoalId: 17
+  },
+  {
+    follower: 8,
+    GoalId: 17
+  },
+  {
+    follower: 12,
+    GoalId: 17
+  },
+  {
+    follower: 13,
+    GoalId: 17
+  }
+]);
