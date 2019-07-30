f41c74d3206030f05be4fc2bdfdc12d2ebf82995
aschaaf <schaaf1091@gmail.com>
2019-07-29 18:33:50 -0600

    testing redirect after login

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,7 +1,6 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect } from "react";
 import { useAuth0 } from "../../react-auth0-spa";
-import Dashboard from "../Dashboard";
+import API from "../../utils/API";
 import josh from "./pics/Josh.jpg";
 import alex from "./pics/Alex.jpg";
 import hunter from "./pics/Hunter.jpg";
@@ -10,158 +9,176 @@ import logo from "../../assets/logo.png";
 import "./style.css";
 
 const Home = () => {
-  const { isAuthenticated } = useAuth0();
+  const { user, isAuthenticated, loginWithRedirect } = useAuth0();
+
+  const getUserProfile = () => {
+    API.getUserByEmail(user.email).then(res => {
+      if (res.data.created === undefined) {
+        window.location.replace(window.location.origin + "/profile");
+      }
+    });
+  };
+
+  useEffect(() => {
+    if (user) {
+      getUserProfile(user.email);
+    }
+  });
 
   return (
     <>
-      {!isAuthenticated && (
-        <>
-          <div className="wrapper">
-            <div className="navBtnDiv">
-              <Link to="/dashboard" className="btn btn-large btn-gold ">
+      <div className="wrapper">
+        <div className="navBtnDiv">
+          {!isAuthenticated && (
+            <>
+              <span
+                className="btn btn-large btn-gold"
+                onClick={() =>
+                  loginWithRedirect({
+                    redirect_uri: window.location.origin
+                  })
+                }
+              >
                 Login
-              </Link>
-              <Link to="/profile" className="btn btn-large btn-blue">
+              </span>
+              <span
+                className="btn btn-large btn-blue"
+                onClick={() =>
+                  loginWithRedirect({
+                    redirect_uri: window.location.origin
+                  })
+                }
+              >
                 Sign Up
-              </Link>
-            </div>
+              </span>
+            </>
+          )}
+        </div>
 
-            <div className="container">
-              <div className="row">
-                <div className="col s12 center-align">
-                  <div className="title_homePage">
-                    <h1>
-                      Goal<span className="den">Den</span>
-                    </h1>
-                    <h5>Share goals & achieve more.</h5>
-                  </div>
-                </div>
+        <div className="container">
+          <div className="row">
+            <div className="col s12 center-align">
+              <div className="title_homePage">
+                <h1>
+                  Goal<span className="den">Den</span>
+                </h1>
+                <h5>Share goals & achieve more.</h5>
               </div>
             </div>
           </div>
+        </div>
+      </div>
 
-          <div className="teamContainer z-depth-3">
+      <div className="teamContainer z-depth-3">
+        <div className="row">
+          <div className="col s12">
+            <h3 className="center-align">
+              <img className="logo" src={logo} alt="Logo" />
+              <br />
+              <span className="brandedText">Our Team</span>
+            </h3>
             <div className="row">
-              <div className="col s12">
-                <h3 className="center-align">
-                  <img className="logo" src={logo} alt="Logo" />
-                  <br />
-                  <span className="brandedText">Our Team</span>
-                </h3>
-                <div className="row">
-                  <div className="col l3 s12">
-                    <div className="card">
-                      <img
-                        className="circle responsive-img teamPic"
-                        src={alex}
-                        alt="Alex"
-                      />
-                      <div className="card-stacked">
-                        <div className="home-card-content">
-                          <p className="picName">Alex</p>
-                        </div>
-                      </div>
+              <div className="col l3 s12">
+                <div className="card">
+                  <img
+                    className="circle responsive-img teamPic"
+                    src={alex}
+                    alt="Alex"
+                  />
+                  <div className="card-stacked">
+                    <div className="home-card-content">
+                      <p className="picName">Alex</p>
                     </div>
                   </div>
-                  <div className="col l3 s12">
-                    <div className="card">
-                      <img
-                        className="circle responsive-img teamPic"
-                        src={hunter}
-                        alt="Hunter"
-                      />
-                      <div className="card-stacked">
-                        <div className="home-card-content">
-                          <p className="picName">Hunter</p>
-                        </div>
-                      </div>
+                </div>
+              </div>
+              <div className="col l3 s12">
+                <div className="card">
+                  <img
+                    className="circle responsive-img teamPic"
+                    src={hunter}
+                    alt="Hunter"
+                  />
+                  <div className="card-stacked">
+                    <div className="home-card-content">
+                      <p className="picName">Hunter</p>
                     </div>
                   </div>
-                  <div className="col l3 s12">
-                    <div className="card">
-                      <img
-                        className="circle responsive-img teamPic"
-                        src={phil}
-                        alt="Phil"
-                      />
-                      <div className="card-stacked">
-                        <div className="home-card-content">
-                          <p className="picName">Phil</p>
-                        </div>
-                      </div>
+                </div>
+              </div>
+              <div className="col l3 s12">
+                <div className="card">
+                  <img
+                    className="circle responsive-img teamPic"
+                    src={phil}
+                    alt="Phil"
+                  />
+                  <div className="card-stacked">
+                    <div className="home-card-content">
+                      <p className="picName">Phil</p>
                     </div>
                   </div>
-                  <div className="col l3 s12">
-                    <div className="card">
-                      <img
-                        className="circle responsive-img teamPic"
-                        src={josh}
-                        alt="Josh"
-                      />
-                      <div className="card-stacked">
-                        <div className="home-card-content">
-                          <p className="picName">Josh</p>
-                        </div>
-                      </div>
+                </div>
+              </div>
+              <div className="col l3 s12">
+                <div className="card">
+                  <img
+                    className="circle responsive-img teamPic"
+                    src={josh}
+                    alt="Josh"
+                  />
+                  <div className="card-stacked">
+                    <div className="home-card-content">
+                      <p className="picName">Josh</p>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          <div className="contactContainer z-depth-3">
+        </div>
+      </div>
+      <div className="contactContainer z-depth-3">
+        <div className="row">
+          <div className="col s8 offset-s2">
+            <h3 className="center-align">
+              <span className="brandedText">Contact</span>
+            </h3>
             <div className="row">
-              <div className="col s8 offset-s2">
-                <h3 className="center-align">
-                  <span className="brandedText">Contact</span>
-                </h3>
-                <div className="row">
-                  <form
-                    action="https://formspree.io/goal.denapp@gmail.com"
-                    method="POST"
-                  >
-                    <div className="input-field">
-                      <input
-                        id="name"
-                        type="text"
-                        name="name"
-                        placeholder="Name"
-                      />
-                    </div>
-                    <div className="input-field ">
-                      <input
-                        id="email"
-                        type="email"
-                        className="form-input"
-                        name="_email"
-                        placeholder="Email"
-                      />
-                    </div>
-                    <div className="input-field">
-                      <textarea
-                        id="message"
-                        className="materialize-textarea"
-                        placeholder="Message"
-                        name="message"
-                      />
-                    </div>
-                    <p className="right-align">
-                      <button
-                        type="submit"
-                        className="btn btn-blue"
-                        name="action"
-                      >
-                        Send Message
-                      </button>
-                    </p>
-                  </form>
+              <form
+                action="https://formspree.io/goal.denapp@gmail.com"
+                method="POST"
+              >
+                <div className="input-field">
+                  <input id="name" type="text" name="name" placeholder="Name" />
                 </div>
-              </div>
+                <div className="input-field ">
+                  <input
+                    id="email"
+                    type="email"
+                    className="form-input"
+                    name="_email"
+                    placeholder="Email"
+                  />
+                </div>
+                <div className="input-field">
+                  <textarea
+                    id="message"
+                    className="materialize-textarea"
+                    placeholder="Message"
+                    name="message"
+                  />
+                </div>
+                <p className="right-align">
+                  <button type="submit" className="btn btn-blue" name="action">
+                    Send Message
+                  </button>
+                </p>
+              </form>
             </div>
           </div>
-        </>
-      )}
-      {isAuthenticated && <Dashboard />}
+        </div>
+      </div>
     </>
   );
 };
